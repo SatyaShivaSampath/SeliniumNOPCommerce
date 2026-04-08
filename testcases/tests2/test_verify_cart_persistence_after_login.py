c02d0521db831850a40b05bf64cf93a05df476b7
jamisaikamal <JamiSai.Kamal@cognizant.com>
2026-04-08 16:55:40 +0530

    Update test_verify_cart_persistence_after_login.py

--- testcases/tests2/test_verify_cart_persistence_after_login.py
+++ testcases/tests2/test_verify_cart_persistence_after_login.py
@@ -1,5 +1,5 @@
-from pages2.my_account_page_verify import login_page_and_my_acc_verify
-from pages2.search_and_cart_page import SearchAndCartPage
+from pages.pages2.my_account_page_verify import login_page_and_my_acc_verify
+from pages.pages2.search_and_cart_page import SearchAndCartPage
 import pytest, time
 
 @pytest.mark.regression
